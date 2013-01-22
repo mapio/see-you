0a277fb0a891c847f5d04d79d5caa5116c11d4e4
Massimo Santini <santini@di.unimi.it>
2013-01-22 03:42:00 +0100

    Renamed makefiles and moved in bundle, valuta is now a python script
    --HG--
    rename : support/Makefile-student => support/Makefile
    rename : support/Makefile-teacher => support/Makefile-prepare
    rename : support/bin/valuta => support/bin/valuta.py

--- support/bin/valuta.py
+++ support/bin/valuta.py
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export MAKEFILES="$(pwd)/Makefile-student $(pwd)/Makefile-teacher"
+uid="$1"
+timestamp="$2"
+
+export MAKEFILES="$(pwd)/Makefile-student $(pwd)/Makefile-minos"
 touch esercizio-*/output-*txt
-make revals exit_on_fail=false >/dev/null 2>&1
+make -k rdiffs >/dev/null 2>&1
 if [ "$(echo ./esercizio-*/eval.txt)" != './esercizio-*/eval.txt' ]; then
 	for es in ./esercizio-*/eval.txt; do
 		n=$(echo $es | sed 's|.*-\(.*\)/.*|\1|' )
