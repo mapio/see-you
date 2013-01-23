9b65088df3a5af86924f2a73605585f0a49ba0a7
Massimo Santini <santini@di.unimi.it>
2013-01-23 11:46:59 +0100

    Absorbing scan in test (by making uid command parameter optional)

--- cu/test.py
+++ cu/test.py
@@ -12,8 +12,8 @@ from .testrunner import TestRunner
 def test( uid, timestamp = None, result_dir = None, clean = None ):
 	if not result_dir: result_dir = UPLOAD_DIR
 	if not timestamp:
-		p = recompile( r'.*/([0-9]+)\.tar' )
-		timestamp = max( p.match( _ ).group( 1 ) for _ in glob( join( UPLOAD_DIR, uid, '*.tar' ) ) )
+		re = recompile( r'.*/([0-9]+)\.tar' )
+		timestamp = max( re.match( _ ).group( 1 ) for _ in glob( join( UPLOAD_DIR, uid, '*.tar' ) ) )
 	dest_dir = join( result_dir, uid, timestamp )
 	if isdir( dest_dir ):
 		if clean: rmtree( dest_dir )
@@ -30,11 +30,13 @@ def test( uid, timestamp = None, result_dir = None, clean = None ):
 if __name__ == '__main__':
 
 	parser = ArgumentParser()
-	parser.add_argument( 'uid', help = 'The UID to test' )
-	parser.add_argument( 'result_dir', help = 'The destination directory where to copy the results directory (default: UPLOAD_DIR)', nargs = '?' )
-	parser.add_argument( 'timestamp', help = 'The timestamp of the upload to test (default: latest)', nargs = '?' )
-	parser.add_argument( '--clean', '-v', default = False, action='store_true', help = 'Whether to clean the destination result directory first' )
+	parser.add_argument( '--uid', help = 'The UID to test (default: all)' )
+	parser.add_argument( '--result_dir', help = 'The destination directory where to copy the results directory (default: UPLOAD_DIR)' )
+	parser.add_argument( '--timestamp', help = 'The timestamp of the upload to test (default: latest)' )
+	parser.add_argument( '--clean', action='store_true', help = 'Whether to clean the destination result directory first' )
 	args = parser.parse_args()
 
-	hrts, dest_dir = test( args.uid, args.timestamp, args.result_dir, args.clean )
-	print ( 'Results for time {1} stored in {0}' if hrts else 'Results alreay present in {0} (use --clean to force re-testing)' ).format( dest_dir, hrts )
+	if not args.uid: re = recompile( r'.*/(.*)/.*\.tar' )
+	for uid in [ args.uid ] if args.uid else set( re.match( _ ).group( 1 ) for _ in glob( join( UPLOAD_DIR, '*', '*.tar' ) ) ):
+		hrts, dest_dir = test( uid, args.timestamp, args.result_dir, args.clean )
+		print ( 'Results for time {1} stored in {0}' if hrts else 'Results alreay present in {0} (use --clean to force re-testing)' ).format( dest_dir, hrts )
