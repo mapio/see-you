5c4d17688c97467b4e69d051ae6067a9a2bd1c14
Massimo Santini <santini@di.unimi.it>
2013-02-11 02:16:06 +0100

    Fixing indentation

--- src/__main__.py
+++ src/__main__.py
@@ -2,9 +2,9 @@ from importlib import import_module
 from sys import argv
 
 if __name__ == '__main__':
-        try:
-                import_module( 'cu.{0}'.format( argv.pop( 1 ) ) ).main()
-        except ( IndexError, ImportError ):
-                print 'usage: cu {test,jsup} ...'
-        except:
-                raise
+	try:
+		import_module( 'cu.{0}'.format( argv.pop( 1 ) ) ).main()
+	except ( IndexError, ImportError ):
+		print 'usage: cu {test,jsup} ...'
+	except:
+		raise
