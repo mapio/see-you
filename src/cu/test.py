921a6834e5debf9399369547ea94bc4143e3d8b8
Massimo Santini <santini@di.unimi.it>
2013-06-03 14:48:25 +0200

    Cosmetic argparse fixes

--- src/cu/test.py
+++ src/cu/test.py
@@ -1,5 +1,4 @@
 from argparse import ArgumentParser
-from json import dumps
 from os import symlink, unlink
 from os.path import join, islink, isdir
 from subprocess import check_call
@@ -21,7 +20,7 @@ def run( uid, timestamp = None, result_dir = None, clean = None ):
 	latest = join( result_dir, uid, 'latest' )
 	if islink( latest ): unlink( latest )
 	symlink( timestamp, latest )
-	return 'saved in {0} by {1}'.format( dest_dir, tr_as_str )
+	return 'saved in {0} by {1}'.format( dest_dir, tr_as_str )
 
 def build( jenkins_workspace, jenkins_cli, uid, timestamp = None ):
 	if not timestamp: timestamp = max( all_timestamps( uid ) )
@@ -38,14 +37,14 @@ def main():
 	parser = ArgumentParser( prog = 'cu test' )
 	parser.add_argument( '--jenkins_cli', help = 'If defined to the local Jenkins cli script, it will be used to issue builds (instead of running them locally)' )
 	parser.add_argument( '--uid', help = 'The UID to test (default: all)' )
-	parser.add_argument( '--result_dir', help = 'The destination directory where to copy the results directory (default: UPLOAD_DIR)' )
+	parser.add_argument( '--results_dir', help = 'The destination directory where to store the results (default: UPLOAD_DIR)' )
 	parser.add_argument( '--timestamp', help = 'The timestamp of the upload to test (default: latest)' )
 	parser.add_argument( '--clean', action='store_true', help = 'Whether to clean the destination result directory first' )
 	args = parser.parse_args()
 
 	if args.jenkins_cli:
 		for uid in [ args.uid ] if args.uid else all_uids():
-			print 'Build for {0}: {1}'.format( uid, build( args.result_dir, args.jenkins_cli, uid, args.timestamp ) )
+			print 'Build for {0}: {1}'.format( uid, build( args.results_dir, args.jenkins_cli, uid, args.timestamp ) )
 	else:
 		for uid in [ args.uid ] if args.uid else all_uids():
-			print 'Test for {0}: {1}'.format( uid, run( uid, args.timestamp, args.result_dir, args.clean ) )
+			print 'Test for {0}: {1}'.format( uid, run( uid, args.timestamp, args.results_dir, args.clean ) )
